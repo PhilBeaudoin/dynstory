303740f1ff4ea13a10c711152bccf0fe9bdc682f
Philippe Beaudoin <beaudoin@chromium.org>
2016-01-05 07:40:12 -0500

    Removed sample code.

--- dynamic-story/handlers.go
+++ dynamic-story/handlers.go
@@ -1,34 +1,24 @@
 package dynstory
 
 import (
-    "html/template"
     "io/ioutil"
     "net/http"
 )
 
 var (
-  guestbookForm []byte
-  signTemplate  = template.Must(template.ParseFiles("dynamic-story/guestbook.html"))
+  mainTemplate []byte
 )
 
 func init() {
-  content, err := ioutil.ReadFile("dynamic-story/guestbookform.html")
+  content, err := ioutil.ReadFile("dynamic-story/mainTemplate.html")
   if err != nil {
     panic(err)
   }
-  guestbookForm = content
+  mainTemplate = content
 
   http.HandleFunc("/", root)
-  http.HandleFunc("/sign", sign)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-  w.Write(guestbookForm)
-}
-
-func sign(w http.ResponseWriter, r *http.Request) {
-    err := signTemplate.Execute(w, r.FormValue("content"))
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+  w.Write(mainTemplate)
 }
